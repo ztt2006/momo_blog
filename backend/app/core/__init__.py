66cffa421d22da3670011913986d5ac40ce7a5b7
ztt2006 <2743537306@qq.com>
2026-04-14 10:15:09 +0800

    feat:初始化

---
+++ backend/app/core/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/4/14 08:58
+@Author  : 2743537306@qqcom
+@File    : __init__.py.py
+"""
---
+++ backend/app/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/4/14 09:09
+@Author  : 2743537306@qqcom
+@File    : main.py
+"""
